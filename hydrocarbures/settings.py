59c266a35f5a5a3dfc39009d522253df7afdddb1
otace1 <49515497+otace1@users.noreply.github.com>
2021-03-29 12:03:17 +0200

    Update settings.py

--- hydrocarbures/settings.py
+++ hydrocarbures/settings.py
@@ -201,6 +201,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+
+    os.path.join(BASE_DIR, 'assets'),
+
+)
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
