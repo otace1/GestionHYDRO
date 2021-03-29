8bdc39917dc056806584747a6ce82f96ae9101b3
otace1 <49515497+otace1@users.noreply.github.com>
2021-03-29 16:50:31 +0200

    Update settings.py

--- hydrocarbures/settings.py
+++ hydrocarbures/settings.py
@@ -191,14 +191,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
@@ -207,11 +202,10 @@ STATICFILES_DIRS = (
 
 )
 
-
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
 
 # Sentry
 
