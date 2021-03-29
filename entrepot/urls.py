18743f33035a6eceef1e63eeddfe4a1b5ff26f0a
cedric <otace1@gmail.com>
2021-03-29 16:31:46 +0200

    Unexpected Indent FIX

--- entrepot/urls.py
+++ entrepot/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-
     path('', views.GestionEchantillonage.tableauechantillonnage, name='entrepot'),
     path('echantilloner/', views.GestionEchantillonage.echantilloner, name='echantilloner'),
     path('rechercher/', views.GestionEchantillonage.rechercheqrcode, name='rechercher'),
@@ -14,6 +12,4 @@ urlpatterns = [
     #Compteur
     path('c1/', views.GestionEchantillonage.c1, name='c1'),
     path('c2/', views.GestionEchantillonage.c2, name='c2')
-
-
 ]
