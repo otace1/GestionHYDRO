0941e1c1aef6f5a5c12f753c4349f83ee2db299c
otace1 <otace1@gmail.com>
2022-08-06 15:39:45 +0200

    requirements.txt update

---
+++ ads/exp_excel.py
@@ -0,0 +1,31 @@
+# Test for faster export to XLSX with PyExcelerate
+from django_pandas.io import read_frame
+import pandas as pd
+from io import BytesIO
+
+
+def export_excel(df):
+    df = pd.read_json(df, convert_dates=['dateheurecargaison'])
+    bn = pd.DataFrame(df)
+
+    with BytesIO() as b:
+        writer = pd.ExcelWriter(b, engine='xlsxwriter')
+        bn.to_excel(writer, sheet_name='Statistiques', index=False)
+        writer.save()
+        filename = 'stat'
+        content_type = 'application/vnd.ms-excel'
+        response = HttpResponse(b.getvalue(), content_type=content_type)
+        response['Content-Disposition'] = 'attachment; filename="' + filename + '.xlsx"'
+        return response
+
+    # a = Dechargement.objects.values(
+    #     'idcargaison__idcargaison__idcargaison__idcargaison__importateur__nomimportateur').annotate(nombrecamions=Count('idcargaison'),nombrecamionsc=Count()
+    #     Gasoil=Sum('gov', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=1)),
+    #     Mogas=Sum('gov', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=2)),
+    #     Jet=Sum('gov', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=3)),
+    #     Petrole=Sum('gov', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=4)),
+    #     Gasoil_D=Sum('gsv', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=1)),
+    #     Mogas_D=Sum('gsv', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=2)),
+    #     Jet_D=Sum('gsv', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=3)),
+    #     Petrole_D=Sum('gsv', filter=Q(idcargaison__idcargaison__idcargaison__idcargaison__produit=4)),
+    #     Total=Sum('gsv')).order_by('-Total')
