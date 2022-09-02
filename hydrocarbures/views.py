2236a2f0d415b33f3017b6df421e5612fc919abc
otace1 <otace1@gmail.com>
2022-09-02 09:41:02 +0200

    API Changes

--- hydrocarbures/views.py
+++ hydrocarbures/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -8,6 +9,11 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         # Add custom claims
+        token["sub"] = settings.SIMPLE_JWT.get("ISSUER", "")
+        # When the serializer is called token['exp'] does not reflect the settings.ACCES_TOKEN_LIFETIME
+        # and is set to now + 1day,thus we subtract a day to get iat
+        token["iat"] = token["exp"] - (60 * 60 * 24)
+        token["claims"] = {"is_superuser": user.is_superuser, "is_staff": user.is_staff}
         token['username'] = user.username
         # token['role'] = user.role
         # ...
