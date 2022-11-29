d96b689fa3cbba860854986cf6674087ff108565
otace1 <otace1@gmail.com>
2022-11-29 16:09:39 +0200

    API for user saved Data

--- api/views.py
+++ api/views.py
@@ -582,9 +582,17 @@ def verificationQrCode(request):
 @permission_classes([IsAuthenticated])
 def showDataSaved(request):
     user = request.user.id
-    data = Cargaison.objects.filter(user=user)
-    serializer = CargaisonSerializer(data, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    c = Cargaison.objects.filter(user=user)
+    list = []
+    for data in c:
+        context = {
+            "dateheurecargaison":data.dateheurecargaison,
+            "importateur":data.importateur.nomimportateur,
+            "entrepot":data.entrepot.nomentrepot,
+            "immatriculation":data.immatriculation,
+        }
+        list.append(context)
+    return Response(list, status=status.HTTP_200_OK)
 
 
 
