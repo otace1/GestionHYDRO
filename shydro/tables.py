a6d4ea5bcc02d45b78e841cbc865a69c77436c40
otace1 <otace1@gmail.com>
2023-07-11 09:47:05 +0200

    Report changes

--- shydro/tables.py
+++ shydro/tables.py
@@ -104,9 +104,9 @@ class CodificationTable(tables.Table):
     class Meta:
         attrs = {
             "class": "table table-bordered table-striped",
-            "id": "example3"
+            "id": "example1"
         }
-        template_name = "django_tables2/bootstrap4-responsive.html"
+        # template_name = "django_tables2/bootstrap4-responsive.html"
 
 
 class ModificationCodification(tables.Table):
