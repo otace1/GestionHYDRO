85a580429866d2df7d42ece4b29b476a968aae31
otace1 <otace1@gmail.com>
2023-07-18 13:37:11 +0200

    UTF8 Encoding

--- api/infiniteScroll.py
+++ api/infiniteScroll.py
@@ -1,7 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+import json
+from datetime import datetime
+
 
 #Paginattion
 class CustomPagination(PageNumberPagination):
     page_size = 4  # Number of items per page
     page_size_query_param = 'page_size'
     max_page_size = 40  # Maximum number of items per page
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
