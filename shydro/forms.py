d9bf12c0375aeac1df3e5c7ab1b4a274a1c7b59b
otace1 <otace1@gmail.com>
2023-07-25 14:52:26 +0200

    DCH

--- shydro/forms.py
+++ shydro/forms.py
@@ -257,28 +257,28 @@ class ImportateurRegularisationForm(forms.ModelForm):
         model = Importateur
         fields = ['idimportateur', 'nomimportateur', 'adresseimportateur']
 
-    def __init__(self, *args, **kwargs):
-        super(ImportateurRegularisationForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'POST'
-        self.helper.form_show_labels = True
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-12'
-        self.helper.field_class = 'col-12'
-        self.helper.layout = Layout(
-            Row("",
-                Column('nomimportateur', css_class='form-group col-12'),
-                css_class='form-row'
-                ),
-            Row("",
-                Column('adresseimportateur', css_class='form-group col-12'),
-                css_class='form-row'
-                ),
-            FormActions(
-                Submit('soumettre', 'soumettre', css_class='btn btn-primary'),
-                Reset('annuler', 'annuler', css_class='btn btn-danger'),
-            ),
-        )
+    # def __init__(self, *args, **kwargs):
+    #     super(ImportateurRegularisationForm, self).__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.form_method = 'POST'
+    #     self.helper.form_show_labels = True
+    #     self.helper.form_class = 'form-horizontal'
+    #     self.helper.label_class = 'col-12'
+    #     self.helper.field_class = 'col-12'
+    #     self.helper.layout = Layout(
+    #         Row("",
+    #             Column('nomimportateur', css_class='form-group col-12'),
+    #             css_class='form-row'
+    #             ),
+    #         Row("",
+    #             Column('adresseimportateur', css_class='form-group col-12'),
+    #             css_class='form-row'
+    #             ),
+    #         FormActions(
+    #             Submit('soumettre', 'soumettre', css_class='btn btn-primary'),
+    #             Reset('annuler', 'annuler', css_class='btn btn-danger'),
+    #         ),
+    #     )
 
 
 # Formulaire pour ajout des entrepots
