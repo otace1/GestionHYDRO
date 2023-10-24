672e10ad4eb5f26485e22fc92fa16ecf6d861040
otace1 <otace1@gmail.com>
2023-10-24 12:33:05 +0100

    pagination INspectionList

--- api/views.py
+++ api/views.py
@@ -874,7 +874,7 @@ def cargaisonInspectionList(request):
     data = Cargaison.objects.filter(etatInspection=True, entrepot__affectationentrepot__username_id=user).order_by('-dateheurecargaison')
 
     # Configure pagination
-    page_size = int(request.GET.get('pagination', 10))  # You can adjust this value according to your preference
+    page_size = int(request.GET.get('pagination', 5))  # You can adjust this value according to your preference
     requested_page = int(request.GET.get('page', 1))
     # Handle the case where requested_page is 0
     if requested_page <= 0:
