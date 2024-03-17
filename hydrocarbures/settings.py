70e0edd48e2daddd6b677429af043d99858e4cec
otace1 <49515497+otace1@users.noreply.github.com>
2024-03-17 20:43:52 +0100

    docker setup for deploy

--- hydrocarbures/settings.py
+++ hydrocarbures/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 
     'debug_toolbar',
     'django_countries',
@@ -315,5 +316,22 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
 # CELERY_TIMEZONE = 'Africa/Lubumbashi'
+
+
+if DEVELOPMENT_MODE is not True:
+    #Static and Media Files Storage
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    AWS_DEFAULT_ACL = os.environ.get("AWS_DEFAULT_ACL")
+    AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_ENDPOINT_URL") # Make sure nyc3 is correct
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400'
+    }
+
+    AWS_STATIC_LOCATION = 'static'
+    STATIC_URL = '%s/%s' % (AWS_S3_ENDPOINT_URL, AWS_STATIC_LOCATION)
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 
