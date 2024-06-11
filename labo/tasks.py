ed8b4bf6831cf91e82258eb47025d93357652612
otace1 <49515497+otace1@users.noreply.github.com>
2024-06-11 12:51:34 +0200

    Fix export Task on Labo for the reports.

--- labo/tasks.py
+++ labo/tasks.py
@@ -41,27 +41,20 @@ def export_report_task(export_format, queryset_data, request=None):
         current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         file_name = f"table_{current_datetime}.{export_format}"
 
-        # Save the exported file to the mounted volume
-        file_path = os.path.join("/vol/web/media", file_name)  # Path to the mounted volume
-        with default_storage.open(file_path, "wb") as destination:
-            destination.write(file_content)
+        # Save the exported file to the default storage (DigitalOcean Spaces)
+        file_path = f"xlsx/{file_name}"
+        file_content_file = ContentFile(file_content)
+        file_url = default_storage.save(file_path, file_content_file)
 
-        # Return the URL of the exported file
-        file_url = os.path.join("/media", file_name)  # Assuming MEDIA_URL is /media/
+        # Get the full URL of the exported file
+        full_file_url = default_storage.url(file_path)
+
+        # Return the full URL of the exported file along with the file name
         return {
-            'file_url': file_url,
+            'file_url': full_file_url,
             'file_name': file_name,
         }
 
-        # # Save the exported file
-        # file_path = default_storage.save(file_name, ContentFile(file_content))
-        # file_url = default_storage.url(file_path)
-        #
-        # # Return the URL of the exported file
-        # return {
-        #     'file_url': file_url,
-        #     'file_name': file_name,
-        # }
     except Exception as e:
         return {'error': str(e)}
 
