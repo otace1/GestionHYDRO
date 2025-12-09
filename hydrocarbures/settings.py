ec06d793cb8808efad99e6cd4c87a1396ff225ad
otace1 <49515497+otace1@users.noreply.github.com>
2025-12-10 00:40:10 +0100

    Setting add used space

--- hydrocarbures/settings.py
+++ hydrocarbures/settings.py
@@ -320,7 +320,7 @@ sentry_sdk.init(
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://trackflow.plus']
+CSRF_TRUSTED_ORIGINS = ['https://trackflow.plus','https://staging.trackflow.plus']
 # CSRF_TRUSTED_ORIGINS = ['https://suivicargo.com']
 
 
