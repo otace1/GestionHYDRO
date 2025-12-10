245d485bdae0c20e004d6e33a52d4c6f8a72c90e
otace1 <49515497+otace1@users.noreply.github.com>
2025-12-10 06:24:33 +0100

    Setting add used space

--- hydrocarbures/cdn/conf.py
+++ hydrocarbures/cdn/conf.py
@@ -6,8 +6,10 @@ AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME="gestionhydro"
 AWS_S3_ENDPOINT_URL="https://sfo3.digitaloceanspaces.com"
-AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-
+AWS_S3_OBJECT_PARAMETERS= {
+    "CacheControl": "max-age=86400",
+    "ACL": "public-read"
+}
 AWS_LOCATION="https://gestionhydro.sfo3.digitaloceanspaces.com"
 
 DEFAULT_FILE_STORAGE="hydrocarbures.cdn.backend.MediaRootS3BotoStorage"
