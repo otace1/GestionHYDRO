afccd8b19cd055a1f5874791eec05b49adadc591
otace1 <49515497+otace1@users.noreply.github.com>
2026-01-26 16:03:43 +0100

    Comment out unused `is_active` field in `accounts/models.py` to prepare for potential removal.

--- accounts/models.py
+++ accounts/models.py
@@ -98,7 +98,7 @@ class MyUser(AbstractBaseUser):
 
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    # is_active = models.BooleanField(default=True)
 
 
 
